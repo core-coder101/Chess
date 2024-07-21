417cece66a919089abacf5c5e4bb557b9a4cd201
core-coder101 <mohsinjawad86@gmail.com>
2024-07-22 01:23:41 +0500

    css for homepage

--- chess/src/components/HomePage.jsx
+++ chess/src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Board from "./Board";
+import "../css/homePage.css"
 
 export default function HomePage() {
   const [scholarsMate, setScholarsMate] = useState([]);
@@ -49,6 +50,7 @@ export default function HomePage() {
       captured: "bp",
     },
   ];
+  const navigate = useNavigate()
   useEffect(() => {
     const interval = setInterval(() => {
       if (count < scholarsMateMoveSequence.length) {
@@ -74,17 +76,23 @@ export default function HomePage() {
 
   return (
     <div
-      className="d-flex align-items-center"
+      className="row homepageOuterDiv"
       style={{ width: "100%", height: "100%" }}
     >
-      <div className="col-6 d-flex align-items-center justify-content-center" style={{ height: "50%", position: "relative" }}>
+      <div className="col-lg-6 col-md-6 col-sm-12 d-flex align-items-center justify-content-center boardOuterDiv">
         <div className="noClickOverlay" style={{position: "absolute", zIndex: "1"}}></div>
             <Board customMoveHistory={scholarsMate} muted />
       </div>
-      <div className="col-6 d-flex align-items-center justify-content-center">
-        <Link to="/play" style={{ color: "white" }}>
-          Play
-        </Link>
+      <div className="col-lg-6 col-md-6 col-sm-12 homepageMenu">
+      <h1 style={{color: "white"}}>Play <span style={{color: "#739552"}}>Chess</span></h1>
+        <div>
+          <button onClick={() => {}} className="hompageButton playOnline">
+            Play Online (Coming Soon)
+          </button>
+          <button onClick={() => navigate("/play")} className="hompageButton playOffline">
+            2-Player Offline
+          </button>
+        </div>
       </div>
     </div>
   );
